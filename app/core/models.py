e95b4b6878f1292599ff80fc884ae0bfdf245c27
Bartłomiej Baryń <b.baryn@moodup.team>
2019-09-04 12:47:49 +0200

    Add simple validate

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 class UserManager(BaseUserManager):
   def create_user(self, email, password=None, **extra_fields):
+    if not email:
+      raise ValueError('User must have email address')
     user = self.model(email=self.normalize_email(email), **extra_fields)
     user.set_password(password)
     user.save(using=self._db)
