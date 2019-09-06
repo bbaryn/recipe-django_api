c38bcf9cf4fa7f0507a83a166790bc6f3672b57f
Bartłomiej Baryń <b.baryn@moodup.team>
2019-09-06 13:05:10 +0200

    Add auth token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -94,6 +94,6 @@ class PublicUserApiTests(TestCase):
 
     def test_create_token_missing_field(self):
         """Test that email and password are required"""
-        res = self.clinet.post(TOKEN_URL, {'email': 'one', 'password': ''})
+        res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
