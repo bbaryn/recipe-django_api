53d5219908d7aed2008f74a75a0424017cd1609d
Bartłomiej Baryń <b.baryn@moodup.team>
2019-09-10 12:01:58 +0200

    Create recipe serilizer and view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag, Ingredients
+from core.models import Tag, Ingredients, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -16,4 +16,23 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredients
         fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredients.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
+                  'price', 'link'
+                  )
         read_only_fields = ('id',)
